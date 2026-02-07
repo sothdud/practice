2396778fe204dfe804568ede09e502bc897666aa
백소영 <soyeong@ati2000.co.kr>
2026-02-07 23:24:20 +0900

    [level 0] Title: l로 만들기, Time: 8.68 ms, Memory: 9.46 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+    for i in myString:
+        if i<'l':
+            answer+='l'
+        else:
+            answer+=i
+            
+        
+    return answer
