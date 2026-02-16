2968249b5bd2be567f841e97e1e8a0625a7fe06f
백소영 <soyeong@ati2000.co.kr>
2026-02-16 22:03:29 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    total=0
+    for i in range(len(absolutes)):
+        if signs[i]:
+            total+=absolutes[i]
+        else:
+            total-=absolutes[i]
+        
+    return total
